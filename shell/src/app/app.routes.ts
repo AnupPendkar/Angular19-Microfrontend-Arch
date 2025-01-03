bb8c7a2006075d266669955ac2de6d2cf7309198
panup <anup.p@atai.ai>
2025-01-03 17:00:18 +0530

    #ADD: Route based feature sharing in mfe.

--- shell/src/app/app.routes.ts
+++ shell/src/app/app.routes.ts
@@ -5,11 +5,18 @@ export const routes: Routes = [
   {
     path: 'remote1',
     loadComponent: () =>
-      loadRemoteModule('remote1', './Component').then((m) => m.RemoteCompComponent),
+      loadRemoteModule('remote1', './Component').then(
+        (m) => m.RemoteCompComponent
+      ),
+  },
+  {
+    path: 'remote2-comp',
+    loadComponent: () =>
+      loadRemoteModule('remote2', './Component').then((m) => m.AppComponent),
   },
   {
     path: 'remote2',
-    loadComponent: () =>
-      loadRemoteModule('remote2', './Component').then((m) => m.AppComponent),
+    loadChildren: () =>
+      loadRemoteModule('remote2', './routes').then((m) => m.routes),
   },
 ];
